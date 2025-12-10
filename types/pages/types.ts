fd18f8e75ec1449d6a7225507553977e9dfdaa7d
csAyushDubey <ayush.dubey@contentstack.com>
2025-12-10 11:59:58 +0530

    refactored routes for composable

--- types/pages/types.ts
+++ types/pages/types.ts
@@ -4,6 +4,10 @@ export interface LocaleParam {
   locale: string;
 }
 
+export interface SlugParam {
+  slug: string[];
+}
+
 export interface PageProps<T> extends React.PropsWithChildren {
   params: Promise<T>;
 }
