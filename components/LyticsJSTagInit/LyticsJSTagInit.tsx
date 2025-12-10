c357a8a0858e8cb625e660fa4e129bf7dbd34fb9
Faraaz Biyabani <faraaz.biyabani@contentstack.com>
2025-12-10 12:57:16 +0530

    feat: [Lytics] jstag.init
    Initialize Lytics using jstag snippet.
    Add sample env with Lytics project ID.

---
+++ components/LyticsJSTagInit/LyticsJSTagInit.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { getLyticsInitSrcUrl } from "@/utils/getLyticsInitSrcUrl";
+import { useEffect } from "react";
+
+export function LyticsJSTagInit() {
+  useEffect(() => {
+    if (!window?.jstag) return;
+    if (window.jstag.isLoaded) return;
+    const projectId = process.env.NEXT_PUBLIC_LYTICS_PROJECT_ID;
+    if (!projectId) {
+      console.warn("NEXT_PUBLIC_LYTICS_PROJECT_ID is not set");
+      return;
+    }
+    window.jstag.init({
+      src: getLyticsInitSrcUrl({
+        projectId,
+      }),
+    });
+  }, []);
+  return null;
+}
